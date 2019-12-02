3cb65c55799a28448e0d8cbb7b00bee7d0bad5b4
Rotem Hemo <rotem@algorand.com>
2019-12-02 09:39:01 -0500

    -- Add support for passing context to handlers -- Change the app to take address and token rather than folder path -- Add support for PureStake API token -- Some PIP8 reformatting

--- algobot.py
+++ algobot.py
@@ -4,7 +4,6 @@
 
 import json
 import logging
-import os
 import signal
 import sys
 
@@ -12,17 +11,6 @@ import algosdk
 
 logger = logging.getLogger(__name__)
 
-# algod = get_algod(os.path.join(os.getenv('HOME'),'Algorand/n3/Node1'))
-# algod = get_algod(os.path.join(os.getenv('HOME'),'mainnet'))
-# print(json.dumps(algod.status(), indent=2))
-# b=algod.block_info(algod.status()['lastRound'])
-# print(json.dumps(b, indent=2))
-def get_algod(algorand_data):
-    addr = open(os.path.join(algorand_data, 'algod.net'), 'rt').read().strip()
-    if not addr.startswith('http'):
-        addr = 'http://' + addr
-    token = open(os.path.join(algorand_data, 'algod.token'), 'rt').read().strip()
-    return algosdk.algod.AlgodClient(token, addr)
 
 # b = nextblock(algod, b['round'])
 def nextblock(algod, lastround=None):
@@ -31,7 +19,7 @@ def nextblock(algod, lastround=None):
         logger.debug('nextblock status lastRound %s', lastround)
     else:
         try:
-            b = algod.block_info(lastround+1)
+            b = algod.block_info(lastround + 1)
             return b
         except:
             pass
@@ -40,9 +28,12 @@ def nextblock(algod, lastround=None):
     b = algod.block_info(nbr)
     return b
 
+
 class Algobot:
-    def __init__(self, algorand_data, block_handlers=None, txn_handlers=None, progress_log_path=None):
-        self.algorand_data = algorand_data
+    def __init__(self, token, address, headres=None, block_handlers=None, txn_handlers=None, progress_log_path=None):
+        self.algod_address = address
+        self.algod_token = token
+        self.algod_headers = headres
         self._algod = None
         self.block_handlers = block_handlers or list()
         self.txn_handlers = txn_handlers or list()
@@ -53,7 +44,7 @@ class Algobot:
 
     def algod(self):
         if self._algod is None:
-            self._algod = get_algod(self.algorand_data)
+            self._algod = algosdk.algod.AlgodClient(self.algod_token, self.algod_address, headers=self.algod_headers)
         return self._algod
 
     def loop(self):
@@ -67,10 +58,11 @@ class Algobot:
             if txns:
                 for txn in txns.get('transactions', []):
                     for th in self.txn_handlers:
-                        th(b, txn)
+                        th(b, txn, {"algod": algod})
             lastround = b['round']
             self.record_block_progress(lastround)
         self.close()
+
     def record_block_progress(self, round_number):
         if self._progresslog is None:
             if self.progress_log_path is None:
@@ -79,6 +71,7 @@ class Algobot:
         # TODO: if the file is very long, start a new one and move-clobber it over
         self._progresslog.write('{}\n'.format(round_number))
         self._progresslog.flush()
+
     def recover_progress(self):
         if self.progress_log_path is None:
             return None
@@ -86,17 +79,19 @@ class Algobot:
             with open(self.progress_log_path, 'rt') as fin:
                 fin.seek(0, 2)
                 endpos = fin.tell()
-                fin.seek(max(0, endpos-100))
+                fin.seek(max(0, endpos - 100))
                 raw = fin.read()
                 lines = raw.splitlines()
                 return int(lines[-1])
         except Exception as e:
             logger.info('could not recover progress: %s', e)
         return None
+
     def close(self):
         if self._progresslog is not None:
             self._progresslog.close()
             self._progresslog = None
+
 
 # block_printer is an example block handler; it takes one arg, the block
 def block_printer(b):
@@ -108,8 +103,9 @@ def block_printer(b):
         if bround % 10 == 0:
             print(bround)
 
+
 # big_tx_printer is an example txn handler; it takes two args, the block and the transaction
-def big_tx_printer(b, tx):
+def big_tx_printer(b, tx, ctx=None):
     payment = tx.get('payment')
     if not payment:
         return
@@ -117,10 +113,13 @@ def big_tx_printer(b, tx):
     if amount > 10000000:
         print(json.dumps(tx, indent=2))
 
+
 def main():
     import argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument('-d', '--algod', default=None, help='algod data dir')
+    ap.add_argument('-a', '--addr', default=None, help='algod addr')
+    ap.add_argument('-t', '--token', default=None, help='algod token')
+    ap.add_argument('--purestake', default=False, help='True if a Purestake token is provided', action='store_true')
     ap.add_argument('--verbose', default=False, action='store_true')
     ap.add_argument('--progress-file', default=None, help='file to write progress to')
     args = ap.parse_args()
@@ -130,19 +129,26 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
 
-    algorand_data = args.algod or os.getenv('ALGORAND_DATA')
-    if not algorand_data:
-        sys.stderr.write('must specify algod data dir by $ALGORAND_DATA or -d/--algod\n')
-        sys.exit(1)
+    # Handle purestake
+    headers = None
+    token = ""
+    if args.token is not None:
+        if not args.purestake:
+            token = args.token
+        else:
+            headers = {"X-API-Key": args.token, }
 
-    #bot = Algobot(algorand_data, block_handlers=[block_printer])
+    # bot = Algobot(algorand_data, block_handlers=[block_printer])
     bot = Algobot(
-        algorand_data,
+        token,
+        args.addr,
+        headers,
         txn_handlers=[big_tx_printer],
         progress_log_path=args.progress_file,
     )
     killcount = [0]
-    def gogently(signum,stackframe):
+
+    def gogently(signum, stackframe):
         count = killcount[0] + 1
         if count == 1:
             sys.stderr.write('signal received. starting graceful shutdown\n')
@@ -151,10 +157,12 @@ def main():
             return
         sys.stderr.write('second signal received. bye\n')
         sys.exit(1)
+
     signal.signal(signal.SIGTERM, gogently)
     signal.signal(signal.SIGINT, gogently)
     bot.loop()
     return
+
 
 if __name__ == '__main__':
     main()
